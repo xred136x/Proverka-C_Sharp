13f754012cbef17c9f1f3abcf133b8df4ba95725
андрей тулинов <tulinof30@yandex.ru>
2023-04-25 13:53:53 +0500

    Добавьте файлы проекта.

---
+++ Proverka C_Sharp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace Proverka_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path;
+            if (args.Length > 0)
+            {
+                path = args[0];
+            }
+            else
+            {
+                path = Environment.CurrentDirectory;
+            }
+            try
+            {
+                path = args[0];
+            }
+            catch (Exception e00)
+            {
+                Console.WriteLine(e00.Message);
+                path = Environment.CurrentDirectory;
+            }
+            DirectoryInfo myDirInfo = new DirectoryInfo(path);
+            foreach (var item in myDirInfo.GetDirectories())
+            {
+                Console.WriteLine($"Каталог {item}");
+            }
+            foreach (var item in myDirInfo.GetFiles())
+            {
+                Console.WriteLine($"Файл {item}");
+               
+            }
+            Console.ReadKey();
+        }
+    }
+    
+}
---
+++ Proverka C_Sharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Proverka C_Sharp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Proverka C_Sharp")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("ac447bb2-66a9-403e-9d9f-245ff2f587ab")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
